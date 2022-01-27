2f11dc7cdc1cb931e604dee0da783ff419250d5d
Federico <gotfede@gmail.com>
2022-01-27 15:39:59 +0000

    add

--- connection.js
+++ connection.js
@@ -1,4 +1,25 @@
+// const { Sequelize } = require("sequelize");
+// const connection = new Sequelize(process.env.DB_URI);
+
+// module.exports = connection;
+
+
 const { Sequelize } = require("sequelize");
-const connection = new Sequelize(process.env.DB_URI);
+
+let connection;
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  connection = new Sequelize(${process.env.DATABASE_URL}?sslmode=no-verify, {
+    url: process.env.DATABASE_URI,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  connection = new Sequelize(process.env.DB_URI);
+}
 
 module.exports = connection;
